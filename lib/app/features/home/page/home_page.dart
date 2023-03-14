e1fb759bf8c1390b87d4c41fc720afd81f4d1ab3
Walicen Dalazuana <walicendalazuana.iblue@ciclic.com.br>
2023-03-14 10:06:36 -0300

    remove app bar

--- lib/app/features/home/page/home_page.dart
+++ lib/app/features/home/page/home_page.dart
@@ -1,16 +1,46 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var currentSelectedIndex = 0;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: Text(
-        'Gods Time',
-        style: Theme.of(context).textTheme.titleLarge,
-      )),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 28.0),
+        child: Row(
+          children: [
+            NavigationRail(
+              destinations: const [
+                NavigationRailDestination(icon: Icon(Icons.home_filled), label: Text('Home')),
+                NavigationRailDestination(
+                    icon: Icon(Icons.menu_book_rounded), label: Text('Bible')),
+              ],
+              selectedIndex: currentSelectedIndex,
+              useIndicator: true,
+              onDestinationSelected: (int index) {
+                debugPrint('index: $index');
+                setState(() {
+                  currentSelectedIndex = index;
+                });
+              },
+            ),
+            const VerticalDivider(thickness: 1, width: 1),
+            // This is the main content.
+            Expanded(
+              child: Center(
+                child: Text('selectedIndex: $currentSelectedIndex'),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
